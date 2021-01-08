98e4eb86f477b5d680db97937431143a014e2391
ognjenstrbanovic <ogistrbanovic@gmail.com>
2021-01-08 16:11:53 -0500

    read data

--- pyramen.py
+++ pyramen.py
@@ -6,33 +6,47 @@ import csv
 from pathlib import Path
 
 # @TODO: Set file paths for menu_data.csv and sales_data.csv
-menu_filepath = Path('')
-sales_filepath = Path('')
+menu_filepath = Path('csv_data/menu_data.csv')
+sales_filepath = Path('csv_data/sales_data.csv')
+
 
 # @TODO: Initialize list objects to hold our menu and sales data
 menu = []
 sales = []
 
+
 # @TODO: Read in the menu data into the menu list
 
+# Open the csv file as an object
+with open(menu_filepath, 'r') as menu_csv_file:
+    # Pass in the csv file to the csv.reader() function
+    # (with ',' as the delmiter/separator) and return the csvreader object
+    menu_csv_reader = csv.reader(menu_csv_file, delimiter=',')
 
+    # Read the header row first (skip this step if there is no header)
+    menu_csv_header = next(menu_csv_reader)
 
+    for row in menu_csv_reader:
+        menu.append(row)
 
-
-
-
-
+print(menu)
 
 
 # @TODO: Read in the sales data into the sales list
 
+# Open the csv file as an object
+with open(sales_filepath, 'r') as sales_csv_file:
+    # Pass in the csv file to the csv.reader() function
+    # (with ',' as the delmiter/separator) and return the csvreader object
+    sales_csv_reader = csv.reader(sales_csv_file, delimiter=',')
 
+    # Read the header row first (skip this step if there is no header)
+    sales_csv_header = next(sales_csv_reader)
 
+    for row in sales_csv_reader:
+        sales.append(row)
 
-
-
-
-
+print(sales)
 
 
 # @TODO: Initialize dict object to hold our key-value pairs of items and metrics
